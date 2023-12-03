c8e491f672b3bc0b8dce39e40cb068aa03ecc28d
Laura Stampf <github@isadora.de>
2023-12-03 12:52:30 +0100

    Day 3 complete

--- src/day3a.py
+++ src/day3a.py
@@ -1,1 +1,65 @@
 file = open("input/day3", "r")
+
+
+def isSymbol(x, y, matrix) -> bool:
+    inBounds: bool = x > -1 and x < len(matrix[0]) and y > -1 and y < len(matrix)
+    return inBounds and matrix[y][x] != '.' and not matrix[y][x].isnumeric()
+
+
+def symbolInRange(coord_range: [(int, int)], matrix: [[chr]]) -> bool:
+    min_x, min_y = coord_range[0]
+    max_x, max_y = coord_range[-1]
+
+    # check left and right
+    if isSymbol(min_x-1, min_y, matrix) or isSymbol(max_x+1, max_y, matrix):
+        return True
+
+    # check above and below
+    for x, y in coord_range:
+        if isSymbol(x, y-1, matrix) or isSymbol(x, y+1, matrix):
+            return True
+
+    # check left diagonal
+    if isSymbol(min_x-1, min_y-1, matrix) or isSymbol(min_x-1, min_y+1, matrix):
+        return True
+
+    # check right diagonal
+    if isSymbol(max_x+1, max_y-1, matrix) or isSymbol(max_x+1, max_y+1, matrix):
+        return True
+
+    return False
+
+
+input_matrix: list[list[chr]] = []
+for line in file:
+    line_list: list[chr] = []
+    for char in line.strip():
+        line_list.append(char)
+
+    input_matrix.append(line_list)
+
+part_numbers: [int] = []
+y: int = 0
+while y < len(input_matrix):
+    x: int = 0
+    while x < len(input_matrix[0]):
+
+        curr_num: int = 0
+        coord_range: (int, int) = []
+
+        if not input_matrix[y][x].isnumeric():
+            x += 1
+            continue
+
+        # locate full number
+        while x < len(input_matrix[0]) and input_matrix[y][x].isnumeric():
+            curr_num = curr_num * 10 + int(input_matrix[y][x])
+            coord_range.append((x, y))
+            x += 1
+
+        # check if symbol is in range
+        if symbolInRange(coord_range=coord_range, matrix=input_matrix):
+            part_numbers.append(curr_num)
+    y += 1
+
+print(sum(part_numbers))
--- src/day3b.py
+++ src/day3b.py
@@ -1,1 +1,95 @@
+from functools import reduce
+
 file = open("input/day3", "r")
+
+
+def is_number(x: int, y: int, matrix: [[chr]]) -> bool:
+    inBounds: bool = x > - \
+        1 and x < len(matrix[0]) and y > -1 and y < len(matrix)
+    return inBounds and matrix[y][x].isnumeric()
+
+
+# returns (number, ending_index of number)
+def get_number(x: int, y: int, matrix: [[chr]]):
+    while is_number(x, y, matrix):
+        x -= 1
+    x += 1
+
+    num: int = 0
+    while (is_number(x, y, matrix)):
+        num = num * 10 + int(matrix[y][x])
+        x += 1
+    # x now points to the next non number
+    return (num, x-1)
+
+
+def find_numbers(x: int, y: int, matrix: [[chr]]) -> [int]:
+    numbers: list = []
+
+    # check left
+    if (is_number(x-1, y, matrix)):
+        num, _ = get_number(x-1, y, matrix)
+        numbers.append(num)
+
+    # check right
+    if (is_number(x+1, y, matrix)):
+        num, _ = get_number(x+1, y, matrix)
+        numbers.append(num)
+
+    # check above
+    xi = x-1
+    if (is_number(x-1, y-1, matrix)):
+        num, xi = get_number(x-1, y-1, matrix)
+        numbers.append(num)
+
+    if (xi < x and is_number(x, y-1, matrix)):
+        num, xi = get_number(x, y-1, matrix)
+        numbers.append(num)
+
+    if (xi < x and is_number(x+1, y-1, matrix)):
+        num, xi = get_number(x+1, y-1, matrix)
+        numbers.append(num)
+
+    # check below
+    xi = x-1
+    if (is_number(x-1, y+1, matrix)):
+        num, xi = get_number(x-1, y+1, matrix)
+        numbers.append(num)
+
+    if (xi < x and is_number(x, y+1, matrix)):
+        num, xi = get_number(x, y+1, matrix)
+        numbers.append(num)
+
+    if (xi < x and is_number(x+1, y+1, matrix)):
+        num, xi = get_number(x+1, y+1, matrix)
+        numbers.append(num)
+
+    return numbers
+
+
+input_matrix: list[list[chr]] = []
+for line in file:
+    line_list: list[chr] = []
+    for char in line.strip():
+        line_list.append(char)
+
+    input_matrix.append(line_list)
+
+y: int = 0
+gear_ratio: int = 0
+while y < len(input_matrix):
+    x: int = 0
+    while x < len(input_matrix[0]):
+
+        if input_matrix[y][x] != '*':
+            x += 1
+            continue
+
+        # find numbers around gear
+        gear_nums: [int] = find_numbers(x, y, input_matrix)
+        if len(gear_nums) == 2:
+            gear_ratio += reduce(lambda x, y: x*y, gear_nums)
+        x += 1
+    y += 1
+
+print(gear_ratio)
