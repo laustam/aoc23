b1a2d9cd11e23de4daee84357280fe4aaf7b9cd5
Laura Stampf <github@isadora.de>
2023-12-08 21:52:32 +0100

    Day 6 complete

--- src/day6a.py
+++ src/day6a.py
@@ -1,1 +1,17 @@
+from functools import reduce
+
 file = open("input/day6", "r")
+
+times: list[int] = [int(num) for num in file.readline().split(':')[1].split(" ") if num.strip()]
+distances: list[int] = [int(num) for num in file.readline().split(':')[1].split(" ") if num.strip()]
+winning_count: list[int] = [0] * len(times)
+
+for i in range(len(times)):
+    time_limit: int = times[i]
+    top_dist: int = distances[i]
+
+    for button_press in range(time_limit):
+        if (button_press * (time_limit - button_press) > top_dist):
+            winning_count[i] += 1
+
+print(reduce(lambda x,y: x*y, winning_count))
--- src/day6b.py
+++ src/day6b.py
@@ -1,1 +1,20 @@
 file = open("input/day6", "r")
+
+time: int = int("".join([num for num in file.readline().split(':')[1].split(" ") if num.strip()]))
+distance: int = int("".join([num for num in file.readline().split(':')[1].split(" ") if num.strip()]))
+
+# find minimum button press
+min_press: int = 0
+for button_press in range(time):
+    if(button_press * (time - button_press) > distance):
+        min_press = button_press
+        break
+
+# find maximum button press
+max_press: int = 0
+for button_press in range(time-1, min_press, -1):
+    if(button_press * (time - button_press) > distance):
+        max_press = button_press
+        break
+
+print(max_press - min_press + 1)
