36b89c16fee2ad6f80aee066c0d7fce0ed7d9806
Laura Stampf <github@isadora.de>
2023-12-08 20:48:01 +0100

    Day 5 complete

--- src/day5a.py
+++ src/day5a.py
@@ -1,1 +1,57 @@
 file = open("input/day5", "r")
+
+class Mapping:
+    soil: int = None
+    fertilizer: int = None
+    water: int = None
+    light: int = None
+    temperature: int = None
+    humidity: int = None
+    location: int = None
+
+    def __init__(self, seed: int) -> None:
+        self.seed:int = seed
+    
+    def in_range(self, attr_name: str, val_range: range) -> (bool, int):
+        attr_val = getattr(self, attr_name)
+        if attr_val not in val_range:
+            return (False, -1)
+        return (True, attr_val - val_range.start)
+
+def put_map(source: str, dest: str, file, mappings: list[Mapping]):
+    file.readline()
+    while True:
+        line: str = file.readline()
+        if not line or line == "\n":
+            break
+
+        dest_val, source_val, length = [int(val) for val in line.strip().split(" ")]
+
+        for mapping in mappings:
+            in_range, offset = mapping.in_range(source, range(source_val, source_val+length))
+            if in_range:
+                setattr(mapping, dest, dest_val + offset)
+
+    for mapping in mappings:
+        source_attr = getattr(mapping, source)
+        if not getattr(mapping, dest):
+            setattr(mapping, dest, source_attr)
+    
+
+mappings: list[Mapping] = []
+
+for initial_seed in file.readline().strip().split(" ")[1:]:
+    new_mapping: Mapping = Mapping(int(initial_seed))
+    mappings.append(new_mapping)
+
+file.readline()
+
+put_map(source="seed", dest="soil", file=file, mappings=mappings)
+put_map(source="soil", dest="fertilizer", file=file, mappings=mappings)
+put_map(source="fertilizer", dest="water", file=file, mappings=mappings)
+put_map(source="water", dest="light", file=file, mappings=mappings)
+put_map(source="light", dest="temperature", file=file, mappings=mappings)
+put_map(source="temperature", dest="humidity", file=file, mappings=mappings)
+put_map(source="humidity", dest="location", file=file, mappings=mappings)
+
+print(min([mapping.location for mapping in mappings]))
--- src/day5b.py
+++ src/day5b.py
@@ -1,1 +1,66 @@
 file = open("input/day5", "r")
+
+def left_overlap(start, end, test_min, test_max): return test_min <= start and test_max > start and test_max < end
+
+def right_overlap(start, end, test_min, test_max): return test_min > start and test_min < end and test_max >= end
+
+def full_partial_overlap(start, end, test_min, test_max): return test_min > start and test_max < end
+
+def full_complete_overlap(start, end, test_min, test_max): return test_min <= start and test_max > start and test_max >= end
+
+def update_ranges(file, values: list[int]):
+    file.readline()
+    new_values: list[int] = []
+    while True:
+        line: str = file.readline()
+
+        if not line or line == "\n":
+            break
+
+        dest_val, source_val, range_len = [int(val) for val in line.strip().split(" ")]
+        offset: int = dest_val - source_val
+
+        i = 0
+        while i < len(values):
+            start, end = values[i]
+            test_min = source_val
+            test_max = source_val + range_len - 1
+
+            # if overlap: (1) add shifted range to new_values then (2) update ranges in original values
+            if (left_overlap(start, end, test_min, test_max)):
+                new_values.append([start+offset, test_max+offset])
+                values[i] = [test_max+1, end]
+
+            elif (right_overlap(start, end, test_min, test_max)):
+                new_values.append([test_min+offset, end+offset])
+                values[i] = [start, test_min-1]
+
+            elif (full_partial_overlap(start, end, test_min, test_max)):
+                new_values.append([test_min+offset, test_max+offset])
+                values[i] = [start, test_min-1]
+                values.append([test_max+1, end])
+
+            elif (full_complete_overlap(start, end, test_min, test_max)):
+                new_values.append([start+offset, end+offset])
+                values.remove(values[i])
+                continue # removed item at i, so want to check same position i again
+
+            i += 1
+
+    return new_values + values
+
+values: list[int] = []
+initial_seeds: list[int] = [int(val) for val in file.readline().strip().split(" ")[1:]]
+
+i = 0
+while i < len(initial_seeds):
+    new_seed, new_range = initial_seeds[i], initial_seeds[i+1]
+    values.append([new_seed, new_seed+new_range-1])
+    i += 2
+
+file.readline()
+
+for _ in range(7):
+    values = update_ranges(file, values)
+
+print(min([item for item, _ in values]))
